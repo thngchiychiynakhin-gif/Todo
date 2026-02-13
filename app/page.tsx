6ae936f1f5b76eb41ae6d1426bcc7df02b7ec07b
Marlon <thngchiychiynakhin@gmail.com>
2026-02-13 14:06:22 +0700

    2

--- app/page.tsx
+++ app/page.tsx
@@ -10,11 +10,14 @@ const IndexPage = ()=>{
   const handleAddTodo = (event) =>{
     setTodos([event.target.value]);
   }
+  const addTodo = () => {
+    setTodos([{id: todos.length + 1, text: todo}, ...todos]);
+  }
   return(
     <>
     <div>{todo}</div>
     <input type="text" onChange={event => setTodo(event.target.value)}/>
-    <button onClick={handleAddTodo}>Add</button>
+    <button onClick={addTodo}>Add</button>
     <ul>
       {todos.map(todo => <li key={todo.id}>{todo.text}</li>)}
     </ul>

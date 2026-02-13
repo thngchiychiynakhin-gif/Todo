f9e133c04425e84b176f44d7fa9b131df0e5cace
Marlon <thngchiychiynakhin@gmail.com>
2026-02-13 15:22:46 +0700

    2

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,69 @@
-'use client';
-import { useState } from 'react';
-const IndexPage = ()=>{
-  const[todo, setTodo] = useState("");
-  const [todos, setTodos] = useState([
-    {id: 1, text: "Todo1"},
-    {id: 2, text: "Todo2"},
-    {id: 3, text: "Todo3"}
-  ]);
-  const handleAddTodo = (event) =>{
-    setTodos([event.target.value]);
+"use client"
+import { type ChangeEventHandler, useState } from 'react'
+
+interface TodoFormProps {
+  addTodo: (todo: string) => void
+}
+
+const TodoForm = ({ addTodo }: TodoFormProps) => {
+  const [todo, setTodo] = useState('')
+  const handleTodoFormChanged: ChangeEventHandler<HTMLInputElement> = (event) => {
+    setTodo(event.target.value)
   }
-  const addTodo = () => {
-    setTodos([{id: todos.length + 1, text: todo}, ...todos]);
+  const handleAddTodo = () => {
+    addTodo(todo)
+    setTodo('')
   }
-  return(
+
+  return (
     <>
-    <div>{todo}</div>
-    <input type="text" onChange={event => setTodo(event.target.value)}/>
-    <button onClick={addTodo}>Add</button>
-    <ul>
-      {todos.map(todo => <li key={todo.id}>{todo.text}</li>)}
-    </ul>
+      <input type="text" onChange={handleTodoFormChanged} value={todo} />
+      <button onClick={handleAddTodo}>Add Todo</button>
     </>
   )
 }
-export default IndexPage;
+
+interface Todo {
+  id: number;
+  content: string;
+}
+
+interface TodoListProps {
+  todos: Todo[]
+}
+
+const TodoList = ({ todos }: TodoListProps) => {
+  return (
+    <ul>
+      {
+        todos.map(todo => <li key={todo.id}>{todo.content}</li>)
+      }
+    </ul>
+  )
+}
+
+const IndexPage = () => {
+  const[isChecked, setIsChecked] = useState(false);
+  const [todos, setTodos] = useState<Todo[]>([
+    { id: 1, content: 'Todo#1' },
+    { id: 2, content: 'Todo#2' },
+    { id: 3, content: 'Todo#3' },
+  ])
+
+  const addTodo = (todo: string) => {
+    setTodos([{ id: todos.length + 1, content: todo }, ...todos])
+  };
+  const handleIsCheck =(event){
+    setIsChecked(event.target.checked);
+  }
+  return (
+    <>
+      <TodoForm addTodo={addTodo}></TodoForm>
+      <TodoList todos={todos}></TodoList>
+      {isChecked && <p>Checked!</p>}
+      <input type="checkbox" name='' id='' onChange={handleIsCheck} />
+    </>
+  )
+}
+
+export default IndexPage
